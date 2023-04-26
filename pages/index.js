f370a5d6739855ad950b13d91b505ac016f51d7f
Arif Yıldırım <arfyldrm0606@gmail.com>
2023-04-26 16:11:13 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -77,8 +77,12 @@ export default function Home() {
   return (
     <>
       <Head>
-        <Header />
+        <title>Valorant Game</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       </Head>
+      <Header />
       <div className="main-container">
         <Sidebar />
         <div className="card-container">
